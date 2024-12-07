7a5ca95f4da8fe34f23d5b6f87f09ae47c81da89
ShlykovPV <ShlykovPV@kamaz.ru>
2024-12-07 19:12:03 +0300

    [Initialization]

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import styles from './Input.module.scss';
+
+interface InputProps {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+}
+
+const Input: FC<InputProps> = ({ value, onChange, placeholder }) => {
+  const hanleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  return (
+<input
+      className={styles.input}
+      value={value}
+      onChange={(e: { target: { value: string; }; }) => onChange(e.target.value)}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default Input;
