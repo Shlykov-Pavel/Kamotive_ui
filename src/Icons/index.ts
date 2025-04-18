9f6e92d880ccfe39b11329e9571df5a3d99839fa
ChumakovaPY <ChumakovaPY@kamaz.ru>
2025-04-18 13:24:25 +0300

    добавлены иконки, исправлено IconButton

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -19,4 +19,5 @@ export { IconColorPicker10 } from './IconColorPicker/IconColorPicker10';
 export { IconFile } from './IconFile/IconFile';
 export { IconUpload } from './IconUpload/IconUpload';
 export { IconDownload } from './IconDownload/IconDownload';
+export { IconAdd } from './IconAdd/IconAdd';
 
