624eec0fcad6c0cda20ae90b452f15e80e97aa04
Shlykovpv <Shlykovpv@kamaz.ru>
2025-06-04 09:38:02 +0300

    [Version 1.2.21]

--- dist/components/Tab/Tab.js
+++ dist/components/Tab/Tab.js
@@ -2,15 +2,16 @@ import React from 'react';
 import styles from './Tab.module.css';
 import classNames from 'classnames';
 import { Typography } from '../Typography/Typography';
-export const Tab = ({ value, onClick, onMouseEnter, label, selected, disabled = false }) => {
+export const Tab = ({ value, onClick, onMouseEnter, label, selected, disabled = false, style, className }) => {
     const handleClick = (e) => {
         if (onClick && value && !disabled) {
             onClick(value);
         }
     };
     return (React.createElement("button", { role: "tab", "aria-selected": selected, "aria-disabled": disabled, value: value, className: classNames(styles.tab, {
-            [styles['selected']]: selected,
-            [styles['disabled']]: disabled,
-        }), onClick: handleClick, onMouseEnter: onMouseEnter },
+            [styles.selected]: selected,
+            [styles.disabled]: disabled,
+            [className || '']: className,
+        }), onClick: handleClick, onMouseEnter: onMouseEnter, style: style },
         React.createElement(Typography, { variant: selected ? 'Body1-SemiBold' : 'Body1' }, label)));
 };
