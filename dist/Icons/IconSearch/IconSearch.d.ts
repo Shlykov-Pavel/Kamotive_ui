820c070f0d8d5fdd17b633d2181c32fa571b74f4
Shlykovpv <Shlykovpv@kamaz.ru>
2025-10-02 14:52:03 +0300

    [Version] 2.10.5

---
+++ dist/Icons/IconSearch/IconSearch.d.ts
@@ -0,0 +1,7 @@
+import { CSSProperties, FC } from 'react';
+export declare const IconSearch: FC<{
+    color?: string;
+    htmlColor?: string;
+    strokeWidth?: number;
+    style?: CSSProperties;
+}>;
