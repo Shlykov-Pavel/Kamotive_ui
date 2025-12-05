ab19f1ea9a740ade48ac370b988933cb238c686c
KirillovaD <sdaria@inbox.ru>
2025-12-05 16:36:24 +0300

    добавлен в FileAttch

--- src/types/index.ts
+++ src/types/index.ts
@@ -413,6 +413,8 @@ export interface FileAttachProps {
   maxFileSize?: number;
   /** Максимальное количество файлов */
   maxFileCount?: number;
+  /** Максимальное количество символов в названии файла */
+  maxFileName?: number;
   /**Поддерживаемые форматы файлов */
   acceptedFormats?: Accept;
   /**Добавленные файлы */
