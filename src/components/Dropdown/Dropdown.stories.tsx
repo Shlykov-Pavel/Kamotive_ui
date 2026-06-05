943940a334ce4fad1ad75aa92645dc91050625fc
Александр Долин <DolinAN@kamaz.ru>
2026-06-05 20:10:04 +0300

    Добавлено правильное описаниеи пример

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -250,7 +250,7 @@ const meta: Meta<typeof Dropdown> = {
       control: { type: 'boolean' },
     },
     preserveSearchValue: {
-      description: 'Позволяет делать поиск по опциям ',
+      description: 'Позволяет сохранить введенное значение при нажатии мимо поля ввода ',
       control: { type: 'boolean' },
     },
     onSearch: {
@@ -927,5 +927,40 @@ DropdownEnglish.args = {
 };
 
 DropdownEnglish.parameters = {
+  controls: { disable: true },
+};
+
+// Dropdown с сохранением значения после воода
+export const DropdownPreserveSearch = (argTypes: DropdownProps<DefaultOption>): JSX.Element => {
+  const [value, setValue] = useState<DefaultOption | null>(null);
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = (val: string) => {
+    // можно ничего не делать или фильтровать локально, если нужно
+    setSearchText(val);
+  };
+
+  const handleChange = (e: any, val: DefaultOption | null) => {
+    setValue(val);
+    setSearchText('');
+  };
+
+  return (
+    <Dropdown
+      {...argTypes}
+      options={dropdownOptions}
+      multiple={false}
+      value={value}
+      onChange={handleChange}
+      enableAutocomplete={true}
+      preserveSearchValue={true}
+      onSearch={handleSearch}
+      placeholder="Начните вводить текст и кликните вне поля"
+      label="Сохранение поискового запроса"
+    />
+  );
+};
+DropdownPreserveSearch.storyName = 'Dropdown с сохранением поиска (preserveSearchValue)';
+DropdownPreserveSearch.parameters = {
   controls: { disable: true },
 };
